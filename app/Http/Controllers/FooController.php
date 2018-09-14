578fb6aa293c882d4dbaad599b95d8db1845138f
Diego Quintale <diego.quintale@betalabs.com.br>
2018-09-14 18:52:12 -0300

    Controller

---
+++ app/Http/Controllers/FooController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Engine;
+
+class FooController extends Controller
+{
+
+    public function index(\Betalabs\Engine\Requests\Request $engineRequest)
+    {
+        // First we need to add the Tenant in the scope. By default it is not
+        // automatic but you can include it in a middleware, for example. Also
+        // since we are using auth:api so an user is surely logged.
+        Engine::auth(\Auth::user());
+
+        $response = $engineRequest->get('engine-endpoint')->getContents();
+        //...
+
+        return response()->setStatusCode(204);
+    }
+
+}
